5302f4a71ab69c8bfb120dac5c6e5cf0b9550594
Taha Jumaah <taharasheed95@gmail.com>
2025-02-04 15:42:27 +0100

    Created and styled a first time visit modal with instructions and player name

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,53 @@
 import GodCard from './GodCard';
 import StandardCard from './StandardCard';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import FirstVisitModal from './Components/FirstVisitModal/FirstVisitModal';
 
+export function updateSessionInfo({
+  playerName,
+  score,
+  highScore,
+  isFirstVisit,
+}) {
+  const playerInfo = {
+    playerName: playerName,
+    score: score,
+    highScore: highScore,
+    isFirstVisit: isFirstVisit,
+  };
+
+  for (const [key, value] of Object.entries(playerInfo)) {
+    sessionStorage.setItem(key, value);
+  }
+}
 export default function App() {
+  useEffect(() => {
+    if (sessionStorage.getItem('isFirstVisit') === 'false') {
+      sessionStorage.setItem('isFirstVisit', 0);
+    }
+  }, []);
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
 
   const [sessionInfo, setSessionInfo] = useState({
-    playerName: '',
-    score: 0,
-    highScore: 0,
-    isFirstVisit: true,
+    playerName: sessionStorage.getItem('playerName'),
+    score: sessionStorage.getItem('playerName'),
+    highScore: sessionStorage.getItem('highScore'),
+    isFirstVisit: !sessionStorage.getItem('isFirstVisit'),
   });
+
+  useEffect(() => {
+    updateSessionInfo(sessionInfo);
+    console.log(sessionStorage);
+  }, [sessionInfo]);
 
   return (
     <>
-      {sessionInfo.isFirstVisit && (
-        <FirstVisitModal isFirstVisit={sessionInfo.isFirstVisit} />
-      )}
+      <FirstVisitModal
+        sessionInfo={sessionInfo}
+        setSessionInfo={setSessionInfo}
+      />
 
       <Header
         changeScore={setScore}
--- src/Components/FirstVisitModal/FirstVisitModal.jsx
+++ src/Components/FirstVisitModal/FirstVisitModal.jsx
@@ -1,19 +1,41 @@
 import { useState } from 'react';
 import styles from './firstVisitModal.module.css';
+import { updateSessionInfo } from '../../App';
 
-export default function firstVisitModal({ isFirstVisit }) {
-  const [isModalShown, setIsModalShown] = useState(isFirstVisit);
+export default function firstVisitModal({ sessionInfo, setSessionInfo }) {
+  const [isModalShown, setIsModalShown] = useState(sessionInfo.isFirstVisit);
+  const [playerName, setPlayerName] = useState(null);
+
+  function handleGameStart(event) {
+    event.preventDefault();
+    console.log(event.target[0].value);
+    setSessionInfo({ ...sessionInfo, isFirstVisit: 0, playerName: playerName });
+  }
   return (
     <div className={isModalShown ? styles.modal_overlay : styles.modal_hidden}>
       <div className={isModalShown ? styles.modal_container : null}>
-        <h2>First VIsit Modal Here</h2>
-        <button
-          onClick={() => {
-            setIsModalShown(false);
-          }}
-        >
-          Start Playing
-        </button>
+        <div className={styles.game_info}>
+          <h2>How to play</h2>
+          <ul>
+            <li>Click on a God image</li>
+            <li>Your score will increase by one</li>
+            <li>The Gods will shuffle their position</li>
+            <li>Your goal is to not click on the same God more than once</li>
+          </ul>
+        </div>
+        <form onSubmit={handleGameStart}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Enter your name or alias"
+            onChange={(event) => {
+              setPlayerName(event.target.value);
+            }}
+          />
+          <button type="submit" className={styles.play_button}>
+            Start Playing
+          </button>
+        </form>
       </div>
     </div>
   );
